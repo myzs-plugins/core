b1298d1eadcf9309390ee5cf1734c30bdbaabe83
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-15 13:21:35 +0700

    Create shell.sh

---
+++ alias/shell.sh
@@ -0,0 +1,11 @@
+# shellcheck disable=SC1090,SC2148
+
+_myzs:internal:module:initial "$0"
+
+_myzs:internal:alias "restart-zsh" "exec zsh"
+_myzs:internal:alias "restart-bash" "exec bash"
+_myzs:internal:alias "restart-fish" "exec fish"
+
+_myzs:internal:alias "restart-shell" "restart-$(_myzs:internal:shell)"
+
+_myzs:internal:alias "reshell" "restart-shell"
