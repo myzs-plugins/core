8c122ea590d3830dbbaefa646404343386f6b0f7
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-04-16 21:58:33 +0700

    chore(release): automatic commit process

--- alias/shell.sh
+++ alias/shell.sh
@@ -9,3 +9,9 @@ _myzs:internal:alias "restart-fish" "exec fish"
 _myzs:internal:alias "restart-shell" "restart-$(_myzs:internal:shell)"
 
 _myzs:internal:alias "reshell" "restart-shell"
+
+speed-shell() {
+  export _MYZS_TYPE="SMALL"
+  restart-shell
+}
+_myzs:internal:alias "spshell" "speed-shell"
