7afb3ab77166c0b43f4240b34a887c45e09e3d19
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-06-09 15:27:43 +0700

    chore(release): automatic commit process

--- alias/shell.sh
+++ alias/shell.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC1090,SC2148
 
-_myzs:internal:module:initial "$0"
+myzs:module:new "$0"
 
 _myzs:internal:alias "restart-zsh" "exec zsh"
 _myzs:internal:alias "restart-bash" "exec bash"
